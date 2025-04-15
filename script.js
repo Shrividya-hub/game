b8984a0cfc14db58e810f3323ce0dbf590c158ec
Shrividya-hub <shrisvidya28@gmail.com>
2025-04-15 16:27:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const catchSound = new Audio("catch.mp3");
+const startSound = new Audio("start.mp3");
+const screens = document.querySelectorAll(".screen");
+const chooseInsectButtons = document.querySelectorAll(".choose-insect-btn");
+const startButton = document.getElementById("start-btn");
+const gameContainer = document.getElementById("game-container");
+const timeElement = document.getElementById("time");
+const scoreElement = document.getElementById("score");
+const message = document.getElementById("message");
+let seconds = 0;
+let score = 0;
+let selectedInsect = {};
+
+startButton.addEventListener("click", () => {
+  startSound.play(); 
+  screens[0].classList.add("up");
+});
+
+
+const increaseScore = () => {
+  score++;
+  if (score > 19) message.classList.add("visible");
+  scoreElement.innerHTML = `Score: ${score}`;
+};
+
+const addInsects = () => {
+  setTimeout(createInsect, 1000);
+  setTimeout(createInsect, 1500);
+};
+
+const catchInsect = function () {
+  increaseScore();
+  catchSound.play();
+  this.classList.add("caught");
+  setTimeout(() => this.remove(), 2000);
+  addInsects();
+};
+
+const getRandomLocation = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+};
+
+const createInsect = () => {
+  const insect = document.createElement("div");
+  insect.classList.add("insect");
+  const { x, y } = getRandomLocation();
+  insect.style.top = `${y}px`;
+  insect.style.left = `${x}px`;
+  insect.innerHTML = `<img src="${selectedInsect.src}" 
+  alt="${selectedInsect.alt}" 
+  style="transform: rotate(${Math.random() * 360}deg)" />`;
+  insect.addEventListener("click", catchInsect);
+  gameContainer.appendChild(insect);
+};
+
+const increaseTime = () => {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeElement.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+};
+
+const bgMusic = new Audio("bg-music.mp3");
+bgMusic.loop = true;
+bgMusic.volume = 0.4; // optional volume control
+
+const startGame = () => {
+  bgMusic.play(); // 🎵 start background music
+  return setInterval(increaseTime, 1000);
+};
+
+chooseInsectButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const image = button.querySelector("img");
+    const src = image.getAttribute("src");
+    const alt = image.getAttribute("alt");
+    selectedInsect = { src, alt };
+    screens[1].classList.add("up");
+    setTimeout(createInsect, 1000);
+    startGame();
+  });
+});
